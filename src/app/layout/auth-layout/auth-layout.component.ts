6135610c0fddf7998ba27b473a5cbb1d5b67cd2e
MK <m.kamel090@gmail.com>
2024-03-07 17:59:08 +0200

    style

--- src/app/layout/auth-layout/auth-layout.component.ts
+++ src/app/layout/auth-layout/auth-layout.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-auth-layout',
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss'
 })
-export class AuthLayoutComponent {
+export class AuthLayoutComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+    document.body.style.background = "url('../../../assets/images/auth-bg.jpg') no-repeat center/cover";
+  }
+
 
 }
--- src/app/layout/content-layout/content-layout.component.ts
+++ src/app/layout/content-layout/content-layout.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content-layout',
   templateUrl: './content-layout.component.html',
   styleUrl: './content-layout.component.scss'
 })
-export class ContentLayoutComponent {
-
+export class ContentLayoutComponent implements OnInit{
+  ngOnInit(): void {
+    document.body.style.background = "#CFE7DF";
+  }
 }
+
